bb6880be81177904b9e46c94672e8f1d01f9dacc
tylersmith <tyjsmith1@gmail.com>
2024-01-02 21:21:34 -0600

    done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Mr. Chocolate",7,"M","Brown","Mean",true);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Rowdy",5,"M","Black","Nice",true);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Tabitha",7,"F","White","Nice",true);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Sergeant Brown",3,"M","Brown","Protective",true);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Melissa",4,"F","Brown","Friendly",true);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Grinch",10,"M","Green","Cranky",true);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Wendy",6,"F","Brown","Shy",true);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES (NULL,1,"M","Black","Shy",true);
--- lib/sql_queries.py
+++ lib/sql_queries.py
@@ -1,18 +1,40 @@
 select_all_female_bears_return_name_and_age = """
-    Write your SQL query here
+    SELECT
+        bears.name,
+        bears.age
+    FROM bears 
+    WHERE sex='F';
 """
 
 select_all_bears_names_and_orders_in_alphabetical_order = """
-    Write your SQL query here
+    SELECT
+        bears.name
+    FROM bears
+    ORDER BY 1 ASC;
 """
 
 select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = """
-    Write your SQL query here
+    SELECT
+        bears.name,
+        bears.age
+    FROM bears
+    WHERE alive = true
+    ORDER by 2 ASC;
 """
 
 select_oldest_bear_and_returns_name_and_age = """
-    Write your SQL query here
+    SELECT
+        bears.name,
+        bears.age
+    FROM bears
+    ORDER BY 2 DESC
+    LIMIT 1;
 """
 select_youngest_bear_and_returns_name_and_age = """
-    Write your SQL query here
+    SELECT
+        bears.name,
+        bears.age
+    FROM bears
+    ORDER BY 2 ASC
+    LIMIT 1;
 """
